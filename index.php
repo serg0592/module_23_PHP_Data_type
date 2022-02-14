5f68b186fd8381a55c866e1310ce8d4474ff08be
Sergey Kudelin <kudelin.work@bk.ru>
2022-02-14 07:14:26 +0300

    Updated: index

--- index.php
+++ index.php
@@ -18,34 +18,10 @@
                 include 'getPerfectPartner.inc.php';
 
                 $rndPerson = $example_persons_array[rand(0, count($example_persons_array) - 1)];
-                /*echo '<pre>';
-                    print_r ($rndPerson);
-                echo '</pre>';*/
-
                 $rndPersonName = $rndPerson['fullname'];
-                /*print_r ($rndPersonName);
-
-                echo '<pre>';*/
-                    $nameParts = getPartsFromFullName($rndPersonName);
-                    /*print_r ($nameParts);
-                echo '</pre>';
-
-                $reName = getFullNameFromParts($nameParts['surName'], $nameParts['firstName'], $nameParts['patronymic']);
-                print_r ($reName);
-                
-                echo '<pre>';
-                    $shortName = getShortName($rndPersonName);
-                    print_r ($shortName);
-                echo '</pre>';
-
-                $rndPersonGender = getGenderFromName($rndPersonName);
-                print_r ($rndPersonGender);
-
-                echo '<pre>';
-                    $genderNum = getGenderDescription($example_persons_array);
-                    print_r ($genderNum);
-                echo '</pre>';*/
-
+                $nameParts = getPartsFromFullName($rndPersonName);
+                $genderNum = getGenderDescription($example_persons_array);
+                print_r ($genderNum);
                 getPerfectPartner($nameParts['surName'], $nameParts['firstName'], $nameParts['patronymic'], $example_persons_array);
             ?>
         </main>
